faf0368578f6571f622d8646d63193038ba7f1bc
SatyamVyas <vyas.satyam.04@gmail.com>
2024-04-14 13:30:32 +0530

    Landing Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import LandingSection from "./LandingSection";
 import RouteRoverLogoText from "./assets/RouteRoverLogoText.svg";
 
+
 function App() {
 	return (
-		<main className="max-w-7xl border-2 border-red-400 m-auto p-8">
-			<img src={RouteRoverLogoText} alt="RouteRover" className="h-8 self-center mb-8"/>
-			<LandingSection />
-		</main>
+		<>
+			<main className="w-dvw h-dvh overflow-x-hidden bg-[#020617]">
+				<img
+					src={RouteRoverLogoText}
+					alt="RouteRover"
+					className="h-8 self-center mt-8 mx-auto"
+				/>
+				<LandingSection />
+			</main>
+		</>
 	);
 }
 
--- src/LandingSection.jsx
+++ src/LandingSection.jsx
@@ -1,30 +1,69 @@
-import { Player, Controls } from "@lottiefiles/react-lottie-player";
-import GIFAnimation from "./assets/LandingAnimation.json";
+import { useEffect } from "react";
+import HeroAsset from "./assets/HeroAsset.png";
+import {
+	motion,
+	useMotionTemplate,
+	useMotionValue,
+	animate,
+} from "framer-motion";
+import { Stars } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
+const COLORS = [
+	"#FF4F00",
+	"#FFBF00",
+	"#00FFBF",
+	"#00BFFF",
+	"#72A0C1",
+	"#B284BE",
+];
 function LandingSection() {
+	const color = useMotionValue(COLORS[0]);
+	const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
+	const border = useMotionTemplate`2px solid ${color}`;
+	const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
+
+	useEffect(() => {
+		animate(color, COLORS, {
+			ease: "easeInOut",
+			duration: 50,
+			repeat: Infinity,
+			repeatType: "mirror",
+		});
+	}, []);
+
 	return (
-		<section className="h-auto sm:h-[70dvh] flex flex-col sm:flex-row items-center justify-center sm:p-16 gap-x-8 gap-y-8">
-			<article className="font-sans h-full flex flex-col justify-center gap-6">
-				<h1 className="text-3xl sm:text-5xl font-bold">
+		<motion.section
+			className=" h-auto md:h-[90dvh] flex flex-col md:flex-row items-center justify-center p-6 md:p-24 gap-x-8 gap-y-8 w-dvw"
+			style={{
+				backgroundImage,
+			}}
+		>
+			<article className="relative z-10 font-sans h-full flex flex-col justify-center gap-8 items-center md:items-start text-center md:text-left">
+				<h1 className="text-3xl md:text-5xl font-bold text-white mt-4 sm:mt-0">
 					Planning your commute was never this Easy!
 				</h1>
-				<p className="text-base sm:text-lg text-zinc-700">
+				<p className="text-base md:text-lg text-zinc-300">
 					Effortlessly explore routes, compare costs, and make
 					informed travel decisions tailored to your preferences and
 					budget
 				</p>
-				<a
+				<motion.a
 					href="#"
-					className="flex items-center justify-center gap-4 group transition-all text-base sm:text-lg w-fit bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full"
+					className="flex items-center justify-center gap-4 group transition-all duration-500 text-base md:text-lg w-fit text-white font-bold py-2 px-4 rounded-full mt-2"
+					style={{
+						border,
+						boxShadow,
+					}}
 				>
 					Get Started with RouteRover{" "}
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						viewBox="0 0 24 24"
-						strokeWidth={1.5}
+						strokeWidth={2}
 						stroke="currentColor"
-						className="w-6 h-6 opacity-0 -translate-x-4 group-hover:opacity-100 group-hover:translate-x-0 transition-all sm:visible invisible"
+						className="w-6 h-6 md:opacity-0 md:-translate-x-4 group-hover:opacity-100 group-hover:translate-x-0 transition-all"
 					>
 						<path
 							strokeLinecap="round"
@@ -32,17 +71,20 @@ function LandingSection() {
 							d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
 						/>
 					</svg>
-				</a>
+				</motion.a>
 			</article>
-			<article>
-				<Player
-					autoplay
-					loop
-					src={GIFAnimation}
-					className="h-full min-w-80 max-w-[900px] backdrop-blur-sm"
-				></Player>
+			<article className="relative z-10">
+				<img
+					src={HeroAsset}
+					className="h-full w-80 sm:w-[400px] md:w-[600px] max-w-[600px]"
+				></img>
 			</article>
-		</section>
+			<div className="z-0 absolute inset-0">
+				<Canvas>
+					<Stars radius={50} count={2500} factor={4} fade speed={2} />
+				</Canvas>
+			</div>
+		</motion.section>
 	);
 }
 
