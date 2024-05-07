cdc597dc68a3389dafa4e90a89685c0f4ee6aaf3
SatyamVyas <vyas.satyam.04@gmail.com>
2024-05-08 00:48:54 +0530

    Updated Features

--- src/Features.jsx
+++ src/Features.jsx
@@ -6,6 +6,10 @@ import {
 } from "framer-motion";
 
 import { useEffect } from "react";
+import Dashboard from "./assets/Dashboad.png";
+import History from "./assets/History.png";
+import Options from "./assets/Options.png";
+import SearchMap from "./assets/SearchMap.png";
 
 const COLORS = ["#00B9E8", "#007FFF", "#6699CC", "#318CE7", "#00BFFF"];
 
@@ -34,37 +38,33 @@ const Features = () => {
 			<div className="mb-4 grid grid-cols-12 gap-4">
 				<BounceCard className="col-span-12 md:col-span-4">
 					<CardTitle>find the FASTEST path...</CardTitle>
-					<div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-violet-400 to-indigo-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
-						<span className="block text-center font-semibold text-indigo-50">
-							FEATURE DEMO HERE
-						</span>
-					</div>
+					<img
+						src={SearchMap}
+						className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-violet-400 to-indigo-400 p-2 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]"
+					></img>
 				</BounceCard>
 				<BounceCard className="col-span-12 md:col-span-8">
 					<CardTitle>that is also CHEAP...</CardTitle>
-					<div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-amber-400 to-orange-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
-						<span className="block text-center font-semibold text-orange-50">
-							FEATURE DEMO HERE
-						</span>
-					</div>
+					<img
+						src={Options}
+						className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-amber-400 to-orange-400 p-2 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]"
+					></img>
 				</BounceCard>
 			</div>
 			<div className="grid grid-cols-12 gap-4">
 				<BounceCard className="col-span-12 md:col-span-8">
 					<CardTitle>and is SAVED in history...</CardTitle>
-					<div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-green-400 to-emerald-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
-						<span className="block text-center font-semibold text-emerald-50">
-							FEATURE DEMO HERE
-						</span>
-					</div>
+					<img
+						src={History}
+						className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-green-400 to-emerald-400 p-2 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]"
+					></img>
 				</BounceCard>
 				<BounceCard className="col-span-12 md:col-span-4">
-					<CardTitle>FREE OF COST</CardTitle>
-					<div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-pink-400 to-red-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
-						<span className="block text-center font-semibold text-red-50">
-							FEATURE DEMO HERE
-						</span>
-					</div>
+					<CardTitle>and ANALYSED</CardTitle>
+					<img
+						src={Dashboard}
+						className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-pink-400 to-red-400 p-2 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]"
+					></img>
 				</BounceCard>
 			</div>
 		</section>
